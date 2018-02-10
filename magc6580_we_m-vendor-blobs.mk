7d3c1b06fbe6943ef74130dc21371544e2a33379
hsagent <hsagent@users.noreply.github.com>
2018-02-10 22:36:21 +0300

    fix

---
+++ magc6580_we_m-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bluboo/magc6580_we_m/bin,system/bin)
